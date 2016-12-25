099596d59eb8aea4dd58dd105524506cfa974af5
Karlen <kmanaseryan@gmail.com>
2016-12-26 01:35:33 +0400

    inception

--- a.js
+++
@@ -1,9 +0,0 @@
-var _import = require('./index').import;
-var _export = require('./index').export;
-
-// _export({
-// 	app: require('./app'),
-// 	client: require('./client_config')
-// });
-// let {RIDE_MODEL_NAME} = _import("RIDE_MODEL_NAME", "CLIENT_MODEL_NAME").from("client");
-// console.log('a: ', RIDE_MODEL_NAME);
